d79465e64a74546a8ea5809e97b74ba67d649d74
hykim <khy2106@naver.com>
2024-04-12 14:16:17 +0900

    0412 handling errors

---
+++ src/app/blog/[postid]/comments/[commentid]/error.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from "react";
+
+export default function errorBoundary() {
+  return (
+    <div>
+      <h1>Error in comment id</h1>
+    </div>
+  );
+}
--- src/app/blog/[postid]/comments/[commentid]/page.tsx
+++ src/app/blog/[postid]/comments/[commentid]/page.tsx
@@ -5,9 +5,19 @@ export default function page({
 }: {
   params: { postid: string; commentid: string };
 }) {
+  const testFunc = (number: number) => {
+    return number + 2;
+  };
+
+  const random = testFunc(5);
+
+  if (random === 7) {
+    throw new Error("에러다");
+  }
   return (
     <div>
       {params.postid} 포스트의 {params.commentid}번째 코멘트!
+      {testFunc(2) === 4}
     </div>
   );
 }
